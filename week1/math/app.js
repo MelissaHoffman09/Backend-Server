b4cdbf4460e3960ebe31caed8e47fc8618979218
Melissa Hoffman <73321224+MelissaHoffman09@users.noreply.github.com>
2021-04-16 14:45:53 -0700

    week1

---
+++ week1/math/app.js
@@ -0,0 +1,13 @@
+const math = require("./math");
+
+console.log(math.add(30, 51));
+// 81
+
+console.log(math.subtract(51, 34));
+// 17
+
+console.log(math.multiply(11, 4));
+// 44
+
+console.log(math.divide(88, 4));
+// 22
